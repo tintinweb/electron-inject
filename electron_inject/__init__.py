abcff918f3ce4c8b2bb7293504171aaf49426100
desophos <1887450+desophos@users.noreply.github.com>
2021-07-22 05:42:20 -0500

    Fix KeyError in `ElectronRemoteDebugger.eval`
    "wasThrown" isn't always present in the JSON response.

--- electron_inject/__init__.py
+++ electron_inject/__init__.py
@@ -93,7 +93,7 @@ class ElectronRemoteDebugger(object):
         ret = json.loads(w['ws'].sendrcv(json.dumps(data)))
         if "result" not in ret:
             return ret
-        if ret['result']['wasThrown']:
+        if ret['result'].get('wasThrown'):
             raise Exception(ret['result']['result'])
         return ret['result']
 
